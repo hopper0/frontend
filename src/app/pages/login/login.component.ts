61c28fa169ca160503535de11ea6c2953c540cb3
andrew.nevsky <andrew.nevsky@yandex.ru>
2023-12-20 22:44:10 +0700

    login.component.ts добавлена страница login page базовые настройки

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterOutlet
+  ],
   template: `
+    <mat-toolbar color="primary">
+      SingUp Student Portal NSTU
+      <button mat-button>
+        <mat-icon>login</mat-icon>
+        Login
+      </button>
+    </mat-toolbar>
     <h1>Страница находится в разработке</h1>
     <p>
       login works! 
     </p>
+    <router-outlet></router-outlet>
   `,
-  styles: ``
+  styles: [`
+    mat-toolbar{
+      justify-content: space-between;
+    }
+  `]
 })
 export class LoginComponent {
 
